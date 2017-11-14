e067ae3ae4e3e7122766f13e748cbd2642a145b2
uday praveen <praveenuday24@gmail.com>
2017-11-14 12:24:27 +0530

    Add files via upload

--- package/src/org/dummy/emp/Emp.java
+++ package/src/org/dummy/emp/Emp.java
@@ -1,30 +1,43 @@
 package org.dummy.emp;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 
 @Entity
+@Table(name = "Employee_Details")
 public class Emp {
 
-	public Emp(int id, String name, String emailId, long phone) {
-		this.id = id;
+	public Emp( String name, String emailId, long phone) {	
 		this.name = name;
 		this.emailId = emailId;
 		this.phone = phone;
 	}
 
 	@Id
+	@GeneratedValue
+	@Column(name = "Emp_Id")
 	private int id;
-	private String name, emailId;
+	@Column(name = "Emp_name", length = 150, nullable = false)
+	private String name;
+	@Column(name = "Emp_mail_id", nullable = false, length = 150)
+	private String emailId;
+	@Column(name = "Emp_phno", length = 10)
 	private long phone;
+	@Column(name = "Basic_Salary")
+	private double basicSalary;
+	@Column(name = "DA", nullable = false, precision = 2)
+	private double dearnessAllowance;
+	@Column(name = "HRA", precision = 2)
+	private double houseREntAllowance;
+	@Column(name = "TA", precision = 2)
+	private double travelAllowance;
+	@Transient
+	private double netSalary;
 
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
 
 	public String getName() {
 		return name;
@@ -49,6 +62,5 @@ public class Emp {
 	public void setPhone(long phone) {
 		this.phone = phone;
 	}
-
 
 }
--- package/src/org/main/Solution.java
+++ package/src/org/main/Solution.java
@@ -12,22 +12,24 @@ import org.hibernate.cfg.Configuration;
 public class Solution {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		SessionFactory sf = new Configuration().configure().buildSessionFactory();
-		Session session=sf.openSession();
-		
-		
+		Session session = sf.openSession();
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		System.out.print("Enter Id:");
-		int id = Integer.valueOf(bf.readLine());
+		int i=3;
+		while(i>0){
+		session.beginTransaction();
 		System.out.print("Enter Name:");
 		String name = bf.readLine();
 		System.out.print("Enter emailId:");
 		String emailId = bf.readLine();
 		System.out.print("Enter phone no:");
 		long phone = Long.valueOf(bf.readLine());
-		Emp employee = new Emp(id, name, emailId, phone);
-		System.out.print("\n Id: " + employee.getId() + "\n Name " + employee.getName() + "\n EmailId: "
-				+ employee.getEmailId() + "\n Phone: " + employee.getPhone() );
-		
+		Emp employee = new Emp(name, emailId, phone);
+		System.out.print( "\n Name " + employee.getName() + "\n EmailId: "
+				+ employee.getEmailId() + "\n Phone: " + employee.getPhone());
+		session.save(employee);
+		session.getTransaction().commit();
+		i--;
+		}
 		session.close();
- }
+	}
 }
